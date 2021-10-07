aa26e4af9ddc29d7b09206bb1a2f66f1ebe90d8d
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-10-07 19:36:19 -0300

    Version 1.06

--- src/components/Projects/ProjectsMain.js
+++ src/components/Projects/ProjectsMain.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function ProjectsMain() {
     return (
         <>
+            <h4><a href="https://rodrigobrentano-annotations-maker.netlify.app/" target="_blank" rel="noreferrer" >Criador de Anotações</a></h4>
             <h4><a href="https://rodrigobrentano-pomodoro.netlify.app" target="_blank" rel="noreferrer" >Pomodoro</a></h4>
             <h4><a href="https://rodrigobrentano-calculator.netlify.app" target="_blank" rel="noreferrer" >Calculadora</a></h4>
             <h4><a href="https://rodrigobrentano-calculator-of-bmi.netlify.app" target="_blank" rel="noreferrer" >Calculadora de IMC</a></h4>
             <h4><a href="https://rodrigobrentano-lista-de-tarefas.netlify.app" target="_blank" rel="noreferrer" >Lista de Tarefas</a></h4>
-            <h4><a href="https://rodrigobrentano-relogio-digital.netlify.app" target="_blank" rel="noreferrer" >Relógio Digital</a></h4>
             <h4><a href="https://rodrigobrentano-timer.netlify.app" target="_blank" rel="noreferrer" >Timer</a></h4>
         </>
     );
