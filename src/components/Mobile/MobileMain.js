0261f07983135f28d602894d549cc2728e00119b
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-10-17 18:21:24 -0300

    Version 1.09

--- src/components/Mobile/MobileMain.js
+++ src/components/Mobile/MobileMain.js
@@ -4,6 +4,7 @@ export default function MobileMain() {
     return (
         <>
             <h4><a href="https://play.google.com/store/apps/details?id=com.ticTacToeJogoDaVelha" target="_blank" rel="noreferrer">Jogo da Velha</a></h4>
+            <h4><a href="https://play.google.com/store/apps/details?id=com.weathercitiesclima" target="_blank" rel="noreferrer">Clima das Cidades</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorCalculadoraCalculator" target="_blank" rel="noreferrer">Calculadora</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorOfImc" target="_blank" rel="noreferrer">Calculadora de IMC</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.gasolinaOuEtanol" target="_blank" rel="noreferrer">Gasolina ou Etanol</a></h4>
