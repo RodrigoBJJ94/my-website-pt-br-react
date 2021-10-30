b0f51a7d9d2cf9b7397341f55fe33d5ad90e4bae
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-10-30 01:08:11 -0300

    Version 1.13

--- src/components/Mobile/MobileMain.js
+++ src/components/Mobile/MobileMain.js
@@ -6,6 +6,7 @@ export default function MobileMain() {
             <h4><a href="https://play.google.com/store/apps/details?id=com.ticTacToeJogoDaVelha" target="_blank" rel="noreferrer">Jogo da Velha</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.weathercitiesclima" target="_blank" rel="noreferrer">Clima das Cidades</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorCalculadoraCalculator" target="_blank" rel="noreferrer">Calculadora</a></h4>
+            <h4><a href="https://play.google.com/store/apps/details?id=com.calories" target="_blank" rel="noreferrer">Contador de Calorias</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.calculatorOfImc" target="_blank" rel="noreferrer">Calculadora de IMC</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.gasolinaOuEtanol" target="_blank" rel="noreferrer">Gasolina ou Etanol</a></h4>
             <h4><a href="https://play.google.com/store/apps/details?id=com.headsOrTailsCaraOuCoroa" target="_blank" rel="noreferrer">Cara ou Coroa</a></h4>
