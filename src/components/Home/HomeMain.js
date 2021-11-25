6179d336c7b65a20db62aa9e335df7032dee3902
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-11-25 18:41:43 -0300

    Version 1.16

--- src/components/Home/HomeMain.js
+++ src/components/Home/HomeMain.js
@@ -4,8 +4,8 @@ export default function HomeMain() {
     return (
         <>
             <p className="tab">Sou um desenvolvedor front-end e mobile, além disso possuo conhecimento em back-end e banco de dados</p>
-            <p className="tab">Possuo conhecimento intermediário nas linguagens JavaScript, HTML, CSS, SCSS e Sass, no framework React Native, na biblioteca ReactJS e em GitHub Desktop</p>
-            <p className="tab">Possuo conhecimento básico nas liguagens TypeScript e Python, nos bancos de dados MongoDB e PostgreSQL, e em NodeJS, Express, Git e Insomnia</p>
+            <p className="tab">Possuo conhecimento intermediário nas linguagens: JavaScript, HTML, CSS, SCSS e Sass, no framework React Native, na biblioteca ReactJS, em Git e GitHub Desktop</p>
+            <p className="tab">Possuo conhecimento básico nas linguagens: TypeScript e Python, nos bancos de dados: MongoDB e PostgreSQL, e em NodeJS, Express e Insomnia</p>
         </>
     );
 };
