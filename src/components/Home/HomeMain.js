9b7603eee61e302eb6d794a3faec70634a8ae810
Rodrigo Brentano <rodrigobjj24@gmail.com>
2022-04-22 01:06:06 -0300

    Version 1.20

--- src/components/Home/HomeMain.js
+++ src/components/Home/HomeMain.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function HomeMain() {
     return (
         <>
-            <p className="tab">Sou um desenvolvedor front-end e mobile, além disso possuo conhecimento em back-end e banco de dados</p>
-            <p className="tab">Possuo conhecimento intermediário nas linguagens: JavaScript, HTML, CSS, SCSS e Sass, no framework React Native, na biblioteca ReactJS, em Git e GitHub Desktop</p>
-            <p className="tab">Possuo conhecimento básico nas linguagens: TypeScript e Python, nos bancos de dados: MongoDB e PostgreSQL, e em Redux, NodeJS, Express e Insomnia</p>
+            <p className="tab">Meu foco é no desenvolvimento front-end e mobile, mas consigo me virar no back-end e banco de dados também</p>
+            <p className="tab">Utilizo diversas linguagens, frameworks, bibliotecas, bancos de dados e outras ferramentas, segue abaixo:</p>
+            <p className="tab">React Native, ReactJS, JavaScript, Laravel, PHP, HTML, CSS, SCSS, Sass, Git, MySQL, PostgreSQL, MongoDB, NodeJS, Express, Redux, Python e TypeScript</p>
         </>
     );
 };
